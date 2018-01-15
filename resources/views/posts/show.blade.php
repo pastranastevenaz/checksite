b93af4106ef4087e005bba440e323ef24fd562e3
Steven Antonio <steven@stevenanton.io>
2018-01-15 16:01:11 -0700

    created edit form for posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,4 +8,9 @@
   </div>
   <hr>
   <small>written on {{$post->created_at->toDayDateTimeString()}}</small>
+  <hr>
+  <a href='{!! url('posts/'.$post->id.'/edit'); !!}' class="btn btn-default">Edit</a>
+
+  <!-- <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a> -->
+
 @endsection
